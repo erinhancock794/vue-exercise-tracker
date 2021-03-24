b5cd283b9e3bd0eb44375ea2b2a88c6854986603
erinhancock794 <erinhancock794@gmail.com>
2021-03-23 19:35:16 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Exercises</h1>
+    <workouts-form @add:item="addItem($event)"/>
+    <exercises :items='exercises'/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Exercises from "./components/Exercises.vue";
+import WorkoutsForm from './components/WorkoutsForm.vue';
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Exercises,
+    WorkoutsForm,
+  },
+  data() {
+    return {
+      exercises: [
+        {
+          id: 1,
+          name: 'Run',
+          time: '30'
+        },
+        {
+          id: 2,
+          name: 'Stretch',
+          time: '15'
+        },
+        ]
+      }
+  },
+  methods: {
+    addItem(item){
+      const lastId = this.exercises.length > 0
+      ? this.exercises[this.exercises.length -1].id
+      : 0;
+
+      const id = lastId + 1;
+      const newItem = { ...item, id};
+      this.exercises = [...this.exercises, newItem]
+    }
+    }
+};
 </script>
 
 <style>
---
+++ src/components/Exercises.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- all the html stuff, components html (like the jsx from react) -->
+  <div id="exerciseList-table">
+    <table>
+        <thead>
+            <tr>
+                <th>Activity</th>
+                <th>Time </th>
+            </tr>
+            <tbody>
+                <tr v-for="item in items" :key="item.id">
+                    <th>{{item.name}}</th>
+                    <th>{{item.time}} minutes</th>
+                </tr>
+               
+            </tbody>
+        </thead>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Exercises',
+    props: {
+        items: Array
+    }
+};
+</script> 
+
+<style scoped>
+</style>
